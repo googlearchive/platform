2e96cd82e295ff4c72254ae045fbdb7d41c4fa0b
Scott Miles <sjmiles@google.com>
2013-04-23 15:27:21 -0700

    update test and smokeTest files

--- test/html/includes/strawkit.js
+++ test/html/includes/strawkit.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2013 The Toolkitchen Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
@@ -8,18 +8,16 @@ Toolkit = {
     if (inElement === window) {
       return;
     }
-    inElement.register({
-      prototype: inPrototype,
-      lifecycle: {
-        readyCallback: function() {
-          var template = inElement.querySelector('template');
-          if (template) {
-            var root = this.webkitCreateShadowRoot();
-            root.appendChild(templateContent(template).cloneNode(true));
-          }
-          inPrototype.created.call(this);
-        }
+    inPrototype.readyCallback = function() {
+      var template = inElement.querySelector('template');
+      if (template) {
+        var root = this.webkitCreateShadowRoot();
+        root.appendChild(templateContent(template).cloneNode(true));
       }
+      inPrototype.created.call(this);
+    };
+    inElement.register({
+      prototype: inPrototype
     });
   }
 };
