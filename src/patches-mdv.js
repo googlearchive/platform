344ffeaae475babb529403f6608588a0fc73f4e7
Steve Orvell <steve@orvellia.com>
2013-07-12 12:04:40 -0700

    __instanceCreated no longer needed:  - cloneNode upgrades  - blink bug 229125 fixed

--- src/patches-mdv.js
+++ src/patches-mdv.js
@@ -11,15 +11,6 @@ style.textContent = 'template {display: none !important;} /* injected by platfor
 var head = document.querySelector('head');
 head.insertBefore(style, head.firstChild);
 
-// MDV hook for processing created dom before bindings are made. We upgrade
-// so custom elements get a chance to deal with bindings mdv is about to make.
-HTMLTemplateElement.__instanceCreated = function(inNode) {
-  // TODO(sorvell): workaround for
-  // https://code.google.com/p/chromium/issues/detail?id=229125
-  document.adoptNode(inNode);
-  CustomElements.upgradeAll(inNode);
-};
-
 // dirtyCheck (with logging)
 function dirtyCheck() {
   logFlags.data && console.group("Model.dirtyCheck()");
