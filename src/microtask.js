b42522c2c5d757815b449dbbd03a51380e5344c3
Steve Orvell <steve@orvellia.com>
2013-08-13 12:04:18 -0700

    fix endOfMicrotask on FF

--- src/microtask.js
+++ src/microtask.js
@@ -7,7 +7,7 @@
 
 var iterations = 0;
 var callbacks = [];
-var twiddle = document.createTextNode();
+var twiddle = document.createTextNode('');
 function eomt(callback) {
   twiddle.textContent = iterations++;
   callbacks.push(callback);
