42b8d164c2834956459fa057d06e589af7d722ca
Scott J. Miles <sjmiles@google.com>
2013-09-11 01:09:49 -0700

    factor microtask.js for easier tracking in profiler/debugger

--- src/microtask.js
+++ src/microtask.js
@@ -8,19 +8,25 @@
 var iterations = 0;
 var callbacks = [];
 var twiddle = document.createTextNode('');
-function eomt(callback) {
+
+function endOfMicrotask(callback) {
   twiddle.textContent = iterations++;
   callbacks.push(callback);
 }
-var MO = window.MutationObserver || window.JsMutationObserver;
-new MO(function() {
+
+function atEndOfMicrotask() {
   while (callbacks.length) {
     callbacks.shift()();
   }
-}).observe(twiddle, {characterData: true});
+}
+
+new (window.MutationObserver || JsMutationObserver)(atEndOfMicrotask)
+  .observe(twiddle, {characterData: true})
+  ;
 
 // exports
-scope.endOfMicrotask = eomt;
 
-})(window.Platform);
+scope.endOfMicrotask = endOfMicrotask;
+
+})(Platform);
 
